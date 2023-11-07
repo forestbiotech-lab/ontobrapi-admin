0f3096d44ec7c49387bc80c5eeac491c46678210
Bruno Costa <brunovasquescosta@gmail.com>
2023-11-07 00:13:35 +0000

    Add tests that are not working because they are try to test weather webpack imports the dependencies or not.

---
+++ src/js/test/main.test.mjs
@@ -0,0 +1,13 @@
+//FAILED TEST
+
+import main from '../main.js'
+
+var expect = require('chai').expect
+
+describe("main.js", () => {
+    describe("Test imports work", () => {
+        it("bootstrap", () => {
+            expect(main.bootstrap).toBeDefined()
+        })
+    })
+})
---
+++ test/webpack.config.test.mjs
@@ -0,0 +1,16 @@
+//Failed test
+
+import bootstrap   from './../public/webpack/bootstrap.js'
+
+//import main  from '../src/js/main.mjs'
+
+
+var expect = require('chai').expect
+
+describe("main.js", () => {
+    describe("Test imports work", () => {
+        it("bootstrap", () => {
+            expect(main.bootstrap).toBeDefined()
+        })
+    })
+})
