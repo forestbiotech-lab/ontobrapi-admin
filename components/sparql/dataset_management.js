9125e6c12cc10b2d9b7a653c4731cbb40c7aaf6f
Bruno Costa <brunovasquescosta@gmail.com>
2024-03-26 14:55:53 +0000

    Add data management call to add dataset,list and get status. Fixes object property problems, and deals with transmission size, by decreasing the size of the payload to query

--- components/sparql/dataset_management.js
+++ components/sparql/dataset_management.js
@@ -1,12 +1,12 @@
 const Query = require('./query')
 
 
-function initialize(repo){
+function init(repo){
     let query=new Query()
-    query.action="INSERT"
     query.graph=`${repo}:`
+    query.action="INSERT"
     query.triples=[
-        "ontobrapi: rdf:type owl:Ontology .",
+        "ontobrapi: rdf:type void:Dataset .",
         "ontobrapi: owl:imports miappe: ."
     ]
     console.log(query.query)
@@ -18,29 +18,12 @@ function add(repo,uid,triples){
         "prefix": `PREFIX dataset: <http://brapi.biodata.pt/${repo}/${uid}#>`,
             "url": `http://brapi.biodata.pt/${repo}/${uid}#`
     }
-
-
-    let query=new Query()
+    let query=new Query(uid)
     query.graph=`${repo}:`
     query.action="INSERT"
     query.loadPrefix("dataset",dataset)
-    query.triples=[
-        "dataset: void:inDataset ontobrapi: .",
-        "dataset: miappe:hasInvestigation dataset:Investigation01 .",
-        //"dataset:Investigation01 rdf:type miappe:investigation .",
-        `dataset:Investigation01 miappe:hasDatabaseId "${uid}"^^xsd:string .`,
-        'dataset:Investigation01 miappe:hasIdentifier "0001"^^xsd:string .',
-        'dataset:Investigation01 miappe:hasName "Investigation 0001"^^xsd:string .',
-        'dataset:hasStatus rdf:type owl:ObjectProperty .',
-        'dataset:hasStatus rdfs:domain dataset:Investigation01 .',
-        'dataset:hasStatus rdfs:range rdfs:Literal .',
-        'dataset:hasStatus rdfs:label "hasStatus" .',
-        'dataset:hasStatus rdfs:subPropertyOf void:hasProperty .',
-        'dataset:hasStatus rdf:type owl:ObjectProperty .',
-        'dataset:Investigation01 miappe:hasStatus "Awaiting validation"^^xsd:string .',
-        'dataset:Investigation01 miappe:hasDescription "Temperature is the major climate element for grapevine development, controlling the rhythm at which several phenological stages like budbreak, flowering, veraison and maturity occur during the biological cycle. In Portugal, information about the thermal requirements of the different cultivars for wine production is lacking. This work aims to evaluate the thermal durations of each development phase of grapevine cultivars of the portuguese collection, located at Estação Vitivinícola Nacional, Dois Portos. Cultivars were classified as short-cycle, medium-cycle or long-cycle according to the duration of both each phase of development and the full cycle. They were also classified as early-season, mid season or late-season cultivars, starting on the 1st of January."^^xsd:string .'
-    ]
-    console.log(query.query)
+    query.triples=triples
+    //console.log(query.query)
     return query.send()
 }
 
@@ -50,18 +33,21 @@ function del(repo){
 
 function get(repo,uid){
     let query=new Query()
-    query.selectors=["?datasets","?dsInvestigation", "?investigationId", "?investigationName", "?investigationStatus", "?investigationDescription"]
+    query.selectors=["?datasets","?dsInvestigation", "?investigationId", "?investigationName", "?investigationStatus", "?investigationDescription", "?studyName"]
     query.graph=`${repo}:`
     query.action="SELECT"
     query.triples=[
         "?datasets void:inDataset ontobrapi: .",
         "?dataset miappe:hasInvestigation ?dsInvestigation .",
-        //"?dsInvestigation rdf:type miappe:investigation .",   //For some reason this does not work, might be the constraint on one ID
+        "?dsInvestigation rdf:type miappe:investigation .",   //For some reason this does not work, might be the constraint on one ID
         `?dsInvestigation miappe:hasDatabaseId "${uid}"^^xsd:string .`,
         "?dsInvestigation miappe:hasIdentifier ?investigationId .",
         "?dsInvestigation miappe:hasName ?investigationName .",
-        "?dsInvestigation miappe:hasStatus ?investigationStatus .",
-        "?dsInvestigation miappe:hasDescription ?investigationDescription ."
+        "?datasets miappe:hasStatus ?investigationStatus .",
+        "?dsInvestigation miappe:hasDescription ?investigationDescription .",
+        "?dsInvestigation miappe:hasPart ?study .",
+        "?study rdf:type miappe:study .",
+        "?study miappe:hasName ?studyName ."
     ]
     console.log(query.query)
     return query.send()
@@ -69,16 +55,17 @@ function get(repo,uid){
 
 function list(repo){
     let query=new Query()
-    query.selectors=["?datasets","?dsInvestigationDbId", "?investigationId", "?investigationName", "?investigationStatus", "?investigationDescription"]
+    query.selectors=["?dataset","?dsInvestigationDbId", "?investigationId", "?investigationName", "?investigationStatus", "?investigationDescription"]
     query.graph=`${repo}:`
     query.action="SELECT"
     query.triples=[
-        "?datasets void:inDataset ontobrapi: .",
+        //Don't need to specify all get all triples associated to Investigation
+        "?dataset void:inDataset ontobrapi: .",
         "?dataset miappe:hasInvestigation ?dsInvestigation .",
         `?dsInvestigation miappe:hasDatabaseId ?dsInvestigationDbId .`,
         "?dsInvestigation miappe:hasIdentifier ?investigationId .",
         "?dsInvestigation miappe:hasName ?investigationName .",
-        "?dsInvestigation miappe:hasStatus ?investigationStatus .",
+        "?dataset miappe:hasStatus ?investigationStatus .",
         "?dsInvestigation miappe:hasDescription ?investigationDescription ."
     ]
     console.log(query.query)
@@ -87,7 +74,7 @@ function list(repo){
 }
 
 module.exports = {
-    initialize,
+    init,
     add,
     del,
     get,
--- components/sparql/query.js
+++ components/sparql/query.js
@@ -4,12 +4,30 @@ const prefixes=require('./assets/prefixes')
 
 
 class Query{
-    constructor(){
+    constructor(uid){
         let host=sparql.host
         let port=sparql.port
         const endpointUrl = `http://${host}:${port}/sparql`
         this.client = new SparqlClient({ endpointUrl })
         this.prefixes=prefixes
+        this._triples=[]
+        if(uid){
+            this._newDataset=[
+                "dataset: void:inDataset ontobrapi: .",
+                "dataset: miappe:hasInvestigation dataset:Investigation?? .",
+                "dataset:Investigation?? rdf:type miappe:investigation .",
+                `dataset:Investigation?? miappe:hasDatabaseId "${uid}"^^xsd:string .`,
+                'dataset:hasStatus rdf:type owl:ObjectProperty .',
+                'dataset:hasStatus rdfs:domain dataset:Investigation?? .',
+                'dataset:hasStatus rdfs:range rdfs:Literal .',
+                'dataset:hasStatus rdfs:label "hasStatus" .',
+                'dataset:hasStatus rdfs:subPropertyOf void:hasProperty .',
+                'dataset:hasStatus rdf:type owl:ObjectProperty .',
+                'dataset: miappe:hasStatus "Awaiting validation"^^xsd:string .' //Default for new dataset
+            ]
+        }else{
+            this._newDataset=[]
+        }
     }
     loadPrefix(key,value){
         this._prefixes[key]=value
@@ -26,6 +44,7 @@ class Query{
         }
         if (input=="INSERT"){
             this._action=`INSERT INTO GRAPH ${this.graph} `
+            this._triples = this._triples.concat(this._newDataset)
         }else if(input=="DELETE"){
             this._action=`DELETE FROM ${this.graph} `
         }else if(input=="SELECT") {
@@ -42,8 +61,55 @@ class Query{
     get action(){
         return this._action
     }
+    convertTerm(term){
+        let prefix = term.split(":",1)[0].slice(1)
+        let instance = decodeURI(term.slice(prefix.length+2,-1).replace(/:/g,"_")).replace(/ /g,"_")
+        let result=instance.matchAll("[^a-zA-Z0-9_-]")
+        if(result){
+            for(let match of result){
+                instance=instance.replace(match[0],`_`)
+            }
+        }
+
+        return `<${this._prefixes[prefix].url}${instance}>`
+    }
     set triples(input){
-        this._triples=input
+        if(input instanceof Array){
+            this._triples=input
+        }else if( input instanceof Object){
+            if(input.individuals && input.properties && input.prefix && input.baseOntology){
+                // Replaces spaces and ":" with "_"
+                let investigation=input.individuals[Object.keys(input.individuals)[0]].s
+                let investigationPrefix=investigation.split(":",1)[0]
+                let investigationInstance=decodeURI(investigation.slice(investigationPrefix.length+1,-1).replace(/:/g,"_")).replace(/ /g,"_")
+                this._triples=this._triples.map(t=>t.replace(/Investigation\?\?/g,investigationInstance))
+
+                let index=0
+                for( let [key,individual] of Object.entries(input.individuals)){
+                    this._triples.push(
+                        `${this.convertTerm(individual.s)} ${individual.p.slice(1,-1)} ${this.convertTerm(individual.o)} .`
+                    )
+                    if(index==5){
+                        break;
+                    }
+                }
+                for( let [key,property] of Object.entries(input.properties)){
+                    if(!property.o.match("\\^\\^")){
+                        property.o=this.convertTerm(property.o)
+                    }
+                    this._triples.push(
+                        `${this.convertTerm(property.s)} ${property.p.slice(1,-1)} ${property.o} .`
+                    )
+                }
+            }else{
+                let undetermined=new Error("undetermined")
+                undetermined.code=500
+                undetermined.message="Undetermined type of triples, must be an array or a triples object"
+                throw  undetermined
+            }
+
+        }
+
     }
     get triples(){
         return this._triples
@@ -79,7 +145,39 @@ class Query{
     set offset(input){
 
     }
-    send(){
+    async send(){
+        if(this.triples.length>50){
+            this._backuptriples=this.triples
+            let queryResult=[]
+            let delta=40
+            let lowerBound=0
+            let upperBound=delta
+
+            for (let i=delta; i<this._backuptriples.length;) {
+                //Can't use set triples (this.triples)
+
+                this._triples = this._backuptriples.slice(lowerBound, upperBound)
+                this.build()
+                console.log("Query size:",new Blob([this.query]).size)
+                let result=await this.send()
+                if(result.err) {
+                    if(delta>10 && result.err.message.startsWith("Bad Request (400): Virtuoso 37000 Error SP030:")){
+                        delta=delta-10
+                        upperBound-=10
+                    }else{
+                        return result
+                    }
+                    console.log("Error size: ",new Blob([result.err.message]).size)
+
+                }else{
+                    queryResult=queryResult.concat(result.data)
+                    lowerBound+=delta
+                    upperBound+=delta
+                    i+=delta
+                }
+            }
+            return {data: queryResult,err:null}
+        }
         return new Promise((res,rej)=> {
             this.client.query.select(this.query).then(stream => {
                 let result = []
