7109921558759c43581fd155cce4e112d043dfe0
Bruno Costa <brunovasquescosta@gmail.com>
2024-03-26 17:48:13 +0000

    Starts testing a KG explorer

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var brapiRouter = require('./routes/brapi');
-var formsRouter = require('./routes/forms');
-var queryRouter = require('./routes/query');
-var factoryRouter = require('./routes/factory');
+const indexRouter = require('./routes/index');
+const brapiRouter = require('./routes/brapi');
+const formsRouter = require('./routes/forms');
+const queryRouter = require('./routes/query');
+const factoryRouter = require('./routes/factory');
+const explorerRouter = require('./routes/explorer');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +29,7 @@ app.use('/admin/brapi', brapiRouter);
 app.use('/admin/forms', formsRouter);
 app.use('/admin/query', queryRouter);
 app.use('/admin/factory', factoryRouter);
+app.use('/', explorerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/explorer.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+//Only receivable routes limited by gatekeeper
+
+
+router.get("/staging/:sdkjkdsjkf", (req, res) => {
+    res.json("ok")
+})
+
+router.get("/ontobrapi/:sdkjkdsjkf", (req, res) => {
+    res.json("ok")
+})
+
