92bc3eb26d36d78d931a6e1cbea071ecbafb80ad
Bruno Costa <brunovasquescosta@gmail.com>
2024-06-18 18:36:07 +0100

    Add retry 3x 5s delay to ensure webserver loaded

--- components/services/initDataset.js
+++ components/services/initDataset.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
 
 const fs=require("fs")
-let env=fs.readFileSync(".env","utf8")
+let retries=0
+function initDataset() {
+    try {
+        let env = fs.readFileSync(".env", "utf8")
 
-let port="3000"
-let portLine=[]
-if(env){
-    portLine = env.split("\n").filter(line=>line.startsWith("PORT"))
+        let port = "3000"
+        let portLine = []
+        if (env) {
+            portLine = env.split("\n").filter(line => line.startsWith("PORT"))
+        }
+        if (portLine.length > 0) {
+            port = portLine[0].split("=")[1]
+        }
+        fetch(`http://localhost:${port}/admin/brapi/dataset/init`).then(response => {
+            response.json().then(json => {
+                console.log(json)
+            })
+
+        })
+    } catch (e) {
+        console.log(e)
+        if (retries < 3) {
+            retries++
+            console.log("Retrying in 5 seconds")
+            setTimeout(function () {
+                initDataset()
+            }, 5000)
+        }
+    }
 }
-if (portLine.length>0){
-    port=portLine[0].split("=")[1]
-}
-console.log(port)
-fetch(`http://localhost:${port}/admin/brapi/dataset/init`).then(response=> {
-    response.json().then(json=>{
-        console.log(json)
-    })
-
-})
-
-
+initDataset()
