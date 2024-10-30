1c55332c4648eac6eb7bf61e096dd57dc57769d6
Bruno Costa <netbofia@hotmail.com>
2024-10-30 18:07:07 +0000

    Updates the service to current standards

--- components/services/updateCalls.js
+++ components/services/updateCalls.js
@@ -1,24 +1,26 @@
 const fs=require('fs')
 const glob=require('glob')
 
+//TODO add version selector
+
 
 function listModules(){
-    let modules=glob.sync('components/modules/*')
+    let modules=glob.sync('components/calls/v2.0/modules/*')
     modules=modules.map(m=>m.split("/").pop())
     return modules
 }
 
 function listCalls(moduleName){
-    let calls=glob.sync(`components/modules/${moduleName}/schemes/*.json`)
+    let calls=glob.sync(`components/calls/v2.0/modules/${moduleName}/schemes/*.json`)
     calls=calls.map(c=>c.split("/").pop())
     return calls
 }
 
 function mapExists(moduleName,callName){
-    if (fs.existsSync(`components/modules/${moduleName}/maps/`)) {
-        return fs.existsSync(`components/modules/${moduleName}/maps/${callName}`)
+    if (fs.existsSync(`components/calls/v2.0/modules/${moduleName}/maps/`)) {
+        return fs.existsSync(`components/calls/v2.0/modules/${moduleName}/maps/${callName}`)
     }else{
-        fs.mkdirSync(`components/modules/${moduleName}/maps`)
+        fs.mkdirSync(`components/calls/v2.0/modules/${moduleName}/maps`)
         return false
     }
 
@@ -27,14 +29,14 @@ function mapExists(moduleName,callName){
 
 function makeMapCall(moduleName,callName) {
     if (!mapExists(moduleName, callName)){
-        fs.copyFileSync(`components/modules/${moduleName}/schemes/${callName}`, `components/modules/${moduleName}/maps/${callName}`)
+        fs.copyFileSync(`components/calls/v2.0/modules/${moduleName}/schemes/${callName}`, `components/modules/${moduleName}/maps/${callName}`)
         let json = JSON.parse(fs.readFileSync(`components/modules/${moduleName}/schemes/${callName}`))
         json["_anchor"] = {}
         json["_anchor"].class = ""
         json["_anchor"].s = ""
         json["_anchor"].p = "rdf:type"
         json["_anchor"].o = ""
-        fs.writeFileSync(`components/modules/${moduleName}/maps/${callName}`, JSON.stringify(json, null,2))
+        fs.writeFileSync(`components/calls/v2.0/modules/${moduleName}/maps/${callName}`, JSON.stringify(json, null,2))
         console.log(`    Map created ${callName}`)
     }
 }
