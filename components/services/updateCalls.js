f9379b9b1cf4abfdcf53784a330253317f106613
Bruno Costa <netbofia@hotmail.com>
2024-11-04 13:29:13 +0000

    Updates updateCalls service to allow versions

--- components/services/updateCalls.js
+++ components/services/updateCalls.js
@@ -1,53 +1,62 @@
 const fs=require('fs')
 const glob=require('glob')
+const utils = require("../db/utils");
 
 //TODO add version selector
 
-
-function listModules(){
-    let modules=glob.sync('components/calls/v2.0/modules/*')
-    modules=modules.map(m=>m.split("/").pop())
+function listVersions(){
+    let modules=glob.sync('components/calls/*/')
+    modules=modules.map(m=>m.slice(0,-1).split("/").pop())
     return modules
 }
 
-function listCalls(moduleName){
-    let calls=glob.sync(`components/calls/v2.0/modules/${moduleName}/schemes/*.json`)
+
+function listModules(version){
+    let modules=glob.sync(`components/calls/${version}/modules/*/`)
+    modules=modules.map(m=>m.slice(0,-1).split("/").pop())
+    return modules
+}
+
+function listCalls(version,moduleName){
+    let calls=glob.sync(`components/calls/${version}/modules/${moduleName}/schemes/*.json`)
     calls=calls.map(c=>c.split("/").pop())
     return calls
 }
 
-function mapExists(moduleName,callName){
-    if (fs.existsSync(`components/calls/v2.0/modules/${moduleName}/maps/`)) {
-        return fs.existsSync(`components/calls/v2.0/modules/${moduleName}/maps/${callName}`)
+function mapExists(version, moduleName,callName){
+    if (fs.existsSync(`components/calls/${version}/modules/${moduleName}/maps/`)) {
+        return fs.existsSync(`components/calls/${version}/modules/${moduleName}/maps/${callName}`)
     }else{
-        fs.mkdirSync(`components/calls/v2.0/modules/${moduleName}/maps`)
+        fs.mkdirSync(`components/calls/${version}/modules/${moduleName}/maps`)
         return false
     }
-
 }
 
 
-function makeMapCall(moduleName,callName) {
-    if (!mapExists(moduleName, callName)){
-        fs.copyFileSync(`components/calls/v2.0/modules/${moduleName}/schemes/${callName}`, `components/modules/${moduleName}/maps/${callName}`)
-        let json = JSON.parse(fs.readFileSync(`components/modules/${moduleName}/schemes/${callName}`))
+
+function makeMapCall(version, moduleName,callName) {
+    if (!mapExists(version, moduleName, callName)){
+        fs.copyFileSync(`components/calls/${version}/modules/${moduleName}/schemes/${callName}`, `components/${version}/modules/${moduleName}/maps/${callName}`)
+        let json = JSON.parse(fs.readFileSync(`components/calls/${version}/modules/${moduleName}/schemes/${callName}`))
         json["_anchor"] = {}
         json["_anchor"].class = ""
         json["_anchor"].s = ""
         json["_anchor"].p = "rdf:type"
         json["_anchor"].o = ""
-        fs.writeFileSync(`components/calls/v2.0/modules/${moduleName}/maps/${callName}`, JSON.stringify(json, null,2))
+        fs.writeFileSync(`components/calls/${version}/modules/${moduleName}/maps/${callName}`, JSON.stringify(json, null,2))
         console.log(`    Map created ${callName}`)
     }
 }
 
 /* Creates maps from schemas if they don't exist */
-modules=listModules()
-for(let module of modules){
-    console.log("Creating maps for "+module)
-    let calls=listCalls(module)
-    for(let call of calls){
-        makeMapCall(module,call)
+for (let version of listVersions()) {
+    for (let module of listModules(version)) {
+        console.log(`Creating maps for: ${module} | version:${version}`)
+        let calls = listCalls(version, module)
+        for (let call of calls) {
+            makeMapCall(version, module, call)
+        }
     }
 }
+
 
